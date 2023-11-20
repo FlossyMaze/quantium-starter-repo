603f3ffcf704290835e6d0b15f7dc00b03bc8720
MaziSS <matheesha2919@gmail.com>
2023-11-21 02:00:55 +0530

    Combined all 3 data files(csv) and made a df with necessary adjustments.
    Made a final file containing the 'sales', 'date', 'region' of the df.

---
+++ read_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+csv_file_paths = ['data/daily_sales_data_0.csv', 'data/daily_sales_data_1.csv', 'data/daily_sales_data_2.csv']
+
+for csv_files in csv_file_paths:
+    df = pd.read_csv(csv_files)
+
+# Only getting the pink morsels
+df = df[df['product'] == 'pink morsel']
+
+# Multiplying price and quantity column
+df['sales'] = df['quantity']*df['price'].apply(lambda x: float(x.replace('$', '')))
+# print(df.head())
+
+# Final File containing 'region', 'date', 'sales'
+task_2_final = df[['sales', 'date', 'region']]
+# print(task_2_final)
