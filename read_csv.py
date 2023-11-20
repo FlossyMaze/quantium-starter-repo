cc031a989965d293dc8a43cb77101abfe4077ab1
MaziSS <matheesha2919@gmail.com>
2023-11-21 02:40:01 +0530

    Combined the 3 csv files to one dataframe.
    Made a 'sales' column multiplying 'price' and 'quantity'.
    
    Created final df with 'sales', 'date', 'region'.
    
    Saved the final df as a csv file

--- read_csv.py
+++ read_csv.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 csv_file_paths = ['data/daily_sales_data_0.csv', 'data/daily_sales_data_1.csv', 'data/daily_sales_data_2.csv']
 
+df3 = []
+
 for csv_files in csv_file_paths:
     df = pd.read_csv(csv_files)
+    df3.append(df)
+
+# Combining all three to one dataframe
+df = pd.concat(df3)
+# print(df.describe())
 
 # Only getting the pink morsels
 df = df[df['product'] == 'pink morsel']
@@ -14,6 +21,6 @@ df['sales'] = df['quantity']*df['price'].apply(lambda x: float(x.replace('$', ''
 
 # Final File containing 'region', 'date', 'sales'
 task_2_final = df[['sales', 'date', 'region']]
-# print(task_2_final)
 
+# print(task_2_final)
 task_2_final.to_csv('task2.csv', index=False)
