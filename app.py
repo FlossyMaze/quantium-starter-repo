b5aaea5cc1a8848f947d670102edcfd493b44709
MaziSS <matheesha2919@gmail.com>
2023-11-23 22:46:51 +0530

    Updated; imported Input, Output and ctx. Created 2 buttons to show all data and show none, from html and callback giving functionality. Added an update_graph function to facilitate button func. Added ids for header and 2 buttons for easy test verification.
    Removed;
    update_layout(updatemenus) created the 2 buttons in the graph.

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, ctx
 import plotly.express as px
 import pandas as pd
-
 
 # Initializing Dashapp
 app = Dash(__name__)
 
-
 # Creating df with task2 csv file
 df = pd.read_csv('task2.csv')
 df['region'] = df["region"].str.title()
-
 
 # Plotting a line chart
 fig = px.line(df, x="date", y="sales",
@@ -22,27 +19,6 @@ fig = px.line(df, x="date", y="sales",
               line_shape='linear',
               template='plotly_dark')
 
-# Create a button to toggle the visibility of all traces
-fig.update_layout(updatemenus=[dict(
-    type="buttons",
-    yanchor='middle',
-    xanchor='left',
-    x=1.01,
-    y=0.45,
-
-    buttons=[dict(label="Show All",
-                  method="update",
-                  args=[{"visible": True},
-                        {"title": "Soul Foods Sales (All Regions)"}]),
-
-             dict(label="Show None",
-                  method="update",
-                  args=[{"visible": "legendonly"},
-                        {"title": "Soul Foods Sales (No Data)"}]),
-
-             ]
-)]
-)
 
 # Color of Chart font
 fig.update_layout(
@@ -52,11 +28,34 @@ fig.update_layout(
 # app layout
 app.layout = html.Div(
     children=[
-        html.H1(
-            children="Soul Foods's Pink Morsel Visualizer"),
+        html.H1("Soul Foods's Pink Morsel Visualizer",
+                id='header'),
         dcc.Graph(id='line-graph', figure=fig),
+        html.Button("Show All", id="show-all-button", n_clicks=0),
+        html.Button("Show None", id="show-none-button", n_clicks=0),
     ],
 )
+
+# Create callbacks to update the figure based on button clicks
+@app.callback(
+    Output('line-graph', 'figure'),
+    Input('show-all-button', 'n_clicks'),
+    Input('show-none-button', 'n_clicks'),
+    prevent_initial_call=True,
+)
+
+
+def update_graph(show_all_clicks, show_none_clicks):
+    button_id = ctx.triggered_id.split('.')[0]  # Get the ID of the clicked button
+    if button_id == 'show-all-button':
+        fig.update_traces(visible=True)
+        fig.update_layout(title="Soul Foods Sales (All Regions)")
+    elif button_id == 'show-none-button':
+        fig.update_traces(visible="legendonly")
+        fig.update_layout(title="Soul Foods Sales (No Data)")
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
